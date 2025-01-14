628d8e849c47bd7c3a36a3c118d7951a83829a0b
Vincent Liu <vintaninja@gmail.com>
2025-01-13 20:22:50 -0500

    removed password

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://vintaninja:Rosegoldpeony@pharmacy-db.qehoq.mongodb.net/pharmacy-db")
+mongoose.connect("mongodb+srv://vintaninja:<password removed in public repo>@pharmacy-db.qehoq.mongodb.net/pharmacy-db")
